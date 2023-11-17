a278b381d8c5b59d2c7109f89d10d1a252a2f638
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-11-17 11:53:26 -0500

    Cambio nombre, actualizacion codigo

---
+++ src/gestorAplicacion/usuario/Usuario.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from gestorAplicacion.administracion.Materia import Materia
+
+
+class Usuario(ABC):
+    _usuariosTotales = []
+
+    def __init__(self, id, nombre, facultad, pw="0000"):
+        self._id = id
+        self._nombre = nombre
+        self._pw = str(pw)
+        self._facultad = facultad
+        Usuario._usuariosTotales.append(self)
+
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @classmethod
+    def mostrarUsuarios(cls) -> str:
+        retorno = ""
+        i = 1
+        for usuario in cls.getUsuariosTotales():
+            retorno += (
+                str(i) + ". " + usuario._nombre + ", id: " + str(usuario._id) + "\n"
+            )
+            i += 1
+        return retorno
+
+    def comprobacionFacultad(self, usuario) -> bool:
+        facultad1 = self.getFacultad().lower()
+        facultad2 = usuario.getFacultad().lower()
+        return facultad1 == facultad2
+
+    def desmatricularDelSistema(usuario) -> None:
+        usuariosTotales = Usuario.getUsuariosTotales()
+        for user in usuariosTotales:
+            if usuario == user:
+                usuariosTotales.remove(usuario)
+                break
+
+    def eliminar_materia(materia) -> None:
+        Materia.getMateriasTotales().remove(materia)
+
+    def agregar_materia(
+        nombre, codigo, descripcion, creditos, facultad, prerrequisitos
+    ) -> None:
+        nuevaMateria = Materia(
+            nombre, codigo, descripcion, creditos, facultad, prerrequisitos
+        )
+        Materia.getMateriasTotales().append(nuevaMateria)
+
+    def getTipo(self):
+        return self._tipo
+    

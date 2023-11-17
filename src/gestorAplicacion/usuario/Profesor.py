cd466fe266aa1a040c63a9d50984c86fd86aeca7
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-11-17 11:46:55 -0500

    clase encontrar profe, gets y sets

--- src/gestorAplicacion/usuario/Profesor.py
+++ src/gestorAplicacion/usuario/Profesor.py
@@ -77,3 +77,57 @@ class Profesor:
             if profesor.daMateria(materia) and profesor not in profes:
                 profes.append(profesor.getNombre())
         return profes
+
+    @classmethod
+    def encontrarProfe(cls, profesor):
+        for profe in Profesor._profesores:
+            if profe.getNombre()== profesor:
+                return profe
+
+    @classmethod
+    def mostrarProfesMateria(cls, nombre):
+        r = ""
+        i = 1
+        profes = cls._profesoresDeMateria(nombre)
+        for profesor in profes:
+            r += f"{i}. {profesor.getNombre()}.\n"
+            i += 1
+        return r
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getFacultad(self):
+        return self._facultad
+
+    def setFacultad(self, facultad):
+        self._facultad = facultad
+
+    def getMateriasDadas(self):
+        return self._materiasDadas
+
+    def setMateriasDadas(self, materiasDadas):
+        self._materiasDadas = materiasDadas
+
+    def getGrupos(self):
+        return self._grupos
+
+    def setGrupos(self, grupos):
+        self._grupos = grupos
+
+    def setHorario(self, horario):
+        self._horario = horario
+
+    def getHorario(self):
+        return self._horario
+
+    @classmethod
+    def getProfesores(cls):
+        return cls._profesores
+
+    @classmethod
+    def setProfesores(cls, profesores):
+        cls._profesores = profesores

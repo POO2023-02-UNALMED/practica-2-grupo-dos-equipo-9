d54826ed7b87c7f3645a33651dea4938be20e9d9
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-11-17 12:09:20 -0500

    mejoras en el codigo de los errores de manejo

--- src/excepciones/ErrorManejo.py
+++ src/excepciones/ErrorManejo.py
@@ -14,28 +14,28 @@ class CampoInvalido(ErrorManejo):
 
 class MateriaMatriculada(ErrorManejo):
     def __init__(self, nombre):
-        super().__init__("El estudiante ya tiene la materia "+nombre+" matriculada")
+        super().__init__(f"El estudiante ya tiene la materia {nombre} matriculada")
 
 class EstudianteSinCreditos(ErrorManejo):
     def __init__(self, nombre=""):
-        super().__init__("El estudiante "+nombre+" no tiene creditos suficientes")
+        super().__init__(f"El estudiante {nombre} no tiene créditos suficientes")
 
 class EstudianteSinMatriculaPagada(ErrorManejo):
     def __init__(self, nombre=""):
-        super().__init__("El estudiante "+nombre+" no tiene la matricula pagada")
+        super().__init__(f"El estudiante {nombre} no tiene la matrícula pagada")
 
 class PrerrequisitosMateria(ErrorManejo):
     def __init__(self, nombre=""):
-        super().__init__("El estudiante "+nombre+" no cumple con los prerrequisitos")
+        super().__init__(f"El estudiante {nombre} no cumple con los prerrequisitos")
 
 class MateriaSinCupo(ErrorManejo):
     def __init__(self, nombre=""):
-        super().__init__("La materia "+nombre+" no cuenta con cupos suficientes")
+        super().__init__(f"La materia {nombre} no cuenta con cupos suficientes")
 
 class GrupoNoAgregado(ErrorManejo):
     def __init__(self):
-        super().__init__("El grupo no ha sido agregado a la materia. El profesor y/o el salón no contaba(n) con disponibilidad")
+        super().__init__("El grupo no ha sido agregado a la materia. El profesor y/o el salón no contaban con disponibilidad")
 
 class BecaExistente(ErrorManejo):
     def __init__(self):
-        super().__init__("El nombre que intenta asignar a la nueva beca, ya existe. Intentelo nuevamente.")
+        super().__init__("El nombre que intenta asignar a la nueva beca ya existe. Inténtelo nuevamente.")

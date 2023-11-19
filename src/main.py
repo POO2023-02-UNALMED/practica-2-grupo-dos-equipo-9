b25c554ef4ceccff86f7ec6066d1042f09745b5d
RicardoLopera <rloperav@unal.edu.co>
2023-11-18 22:39:44 -0500

    suma del main e init en el programa

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from gestorGrafico.ventInicio import VentInicio
+from gestorGrafico.ventLog import VentLog
+from gestorGrafico.ventPrincipal import VentPrincipal
+from gestorGrafico.GenerarHorario import GenerarHorario
+
+from gestorAplicacion.administracion.Beca import Beca
+from gestorAplicacion.administracion.Grupo import Grupo
+from gestorAplicacion.administracion.Horario import Horario
+from gestorAplicacion.administracion.Materia import Materia
+from gestorAplicacion.administracion.Salon import Salon
+from gestorAplicacion.usuario.Coordinador import Coordinador
+from gestorAplicacion.usuario.Estudiante import Estudiante
+from gestorAplicacion.usuario.Profesor import Profesor
+from gestorAplicacion.usuario.Usuario import Usuario
+
+from baseDatos.Serializador import Serializador
+from baseDatos.Deserializador import Deserializador 
+
+
+class MainWin(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Sistema Matricula de Materias")
+        self.resizable(0,0)
+        self.geometry("865x480")
+        self.iconbitmap("Python\src\gestorGrafico\Imagenes\icono.ico")
+        
+        
+        Coordinador.setUsuarioIngresado(None)
+        VentInicio(self)
+        self.mainloop()
+
+    def abrirLog(self):
+        self.destroy()         
+        VentLog()
+    
+
+if __name__=="__main__":
+
+    Deserializador.deserializarDatos()
+
+
+    MainWin()

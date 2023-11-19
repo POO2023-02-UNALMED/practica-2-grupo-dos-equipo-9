29eb82191cccdade6fd1b56168b16b229a7f3fa0
RicardoLopera <rloperav@unal.edu.co>
2023-11-19 16:47:18 -0500

    asi es creo

--- src/gestorGrafico/ventPrincipal.py
+++ src/gestorGrafico/ventPrincipal.py
@@ -1,15 +1,20 @@
-from tkinter import *
 from tkinter import messagebox
+from tkinter import *
 from gestorGrafico.DesmatricularAlumno import DesmatricularAlumno
 from gestorGrafico.MatricularMateria import MatricularMateria
 from gestorGrafico.PostulacionBecas import PostulacionBecas
 from gestorGrafico.GenerarHorario import GenerarHorario
 from gestorGrafico.eliminarAgregarMateria import EliminarAgregarMateria
 
-
 class VentPrincipal(Tk):
     def __init__(self):
         super().__init__()
+        self.configure_window()
+        self.create_menu()
+        self.create_main_frame()
+        self.mainloop()
+
+    def configure_window(self):
         self.title("SDIAcademica")
         self.resizable(0, 0)
         self.geometry("865x480")
@@ -17,100 +22,37 @@ class VentPrincipal(Tk):
         self.pack_propagate(False)
         self.iconphoto(True, PhotoImage(file="/home/Rick/GitHub/practica-2-grupo-dos-equipo-9/src/gestorGrafico/Imagenes/iconoRY.png"))
 
-        def infoBasica():
-            texto = """Bienvenido a nuestra aplicación de Matrícula de Materias.
-
-Nuestra aplicación te permite gestionar de manera eficiente y sencilla el proceso de matrícula de materias en tu institución educativa. Con nuestra aplicación, podrás realizar las siguientes acciones:
-
-1. Matricular materia: Selecciona al estudiante la materias que desea cursar en el próximo período académico y confirma su inscripción.
-
-2. Generar horario: Crea automáticamente un horario de clases basado en las materias que selecciones al estudiante, teniendo en cuenta los horarios disponibles y las restricciones establecidas.
-
-3. Agregar o eliminar materia/grupo: Realiza cambios en la base de datos de materias, ya sea para agregar nuevas materias o eliminar aquellas que ya no deseas que esten en el sistema.
-
-4. Desmatricular alumno: En caso de que necesites cancelar una matrícula en su totalidad, puedes utilizar esta opción para desmatricular de todas las materias inscritas.
-
-5. Búsqueda y postulación de becas: Explora las becas disponibles y postula estudiantes que cumplan con sus requisitos y objetivos académicos.
-
-Estamos comprometidos con brindarte una experiencia fácil de usar y confiable. Si tienes alguna pregunta o necesitas ayuda, no dudes en contactar a nuestro equipo de soporte técnico.
-
-¡Esperamos que disfrutes de nuestra aplicación y tengas un exitoso período académico!
-            """
-            messagebox.showinfo(
-                "Informacion de la aplicacion", texto
-            )
-
-        def salir():
-            self.destroy()
-
-        def mostrarAutores():
-            autores = (
-                "Autores de la aplicacion:\n\n"
-                + "Yiduar Duvier Rangel Quintero\n\n"
-                + "Ricardo Esteban Lopera Vasco\n\n"
-            )
-
-            messagebox.showinfo("Autores", autores)
-
-        def matricularMateria():
-            mataHijos(self)
-            MatricularMateria(self).pack()
-
-        def generarHorario():
-            mataHijos(self)
-            GenerarHorario(self).pack()
-
-        def eliminarAgregarMateria():
-            mataHijos(self)
-            EliminarAgregarMateria(self).pack()
-            
-
-        def desmatricularAlumno():
-            mataHijos(self)
-            DesmatricularAlumno(self).pack()
-
-        def becas():
-            mataHijos(self)
-            PostulacionBecas(self).pack()
-        
-        def mataHijos(ventana):
-            for widget in ventana.winfo_children():
-                if isinstance(widget, Frame):
-                    widget.destroy()
-
+    def create_menu(self):
         menuBar = Menu(self)
-        self.option_add("*tearOff",  False)
+        self.option_add("*tearOff", False)
         self.config(menu=menuBar)
 
         menu1 = Menu(menuBar)
         menuBar.add_cascade(label="Archivo", menu=menu1)
-
-        menu1.add_cascade(label="Aplicacion", command=infoBasica)
-        menu1.add_cascade(label="Salir", command=salir)
+        menu1.add_cascade(label="Aplicacion", command=self.info_basica)
+        menu1.add_cascade(label="Salir", command=self.salir)
 
         menu2 = Menu(menuBar)
         menuBar.add_cascade(label="Procesos y Consultas", menu=menu2)
-
-        menu2.add_cascade(label="Matricular materia", command=matricularMateria)
-        menu2.add_cascade(label="Generar Horario", command=generarHorario)
-        menu2.add_cascade(label="Eliminar o agregar Materia / Grupo", command=eliminarAgregarMateria)
-        menu2.add_cascade(label="Desmatricular Alumno", command=desmatricularAlumno)
-        menu2.add_cascade(label="Busqueda y Postulacion de Becas", command=becas)
+        menu2.add_cascade(label="Matricular materia", command=self.matricular_materia)
+        menu2.add_cascade(label="Generar Horario", command=self.generar_horario)
+        menu2.add_cascade(label="Eliminar o agregar Materia / Grupo", command=self.eliminar_agregar_materia)
+        menu2.add_cascade(label="Desmatricular Alumno", command=self.desmatricular_alumno)
+        menu2.add_cascade(label="Busqueda y Postulacion de Becas", command=self.becas)
 
         menu3 = Menu(menuBar)
         menuBar.add_cascade(label="Ayuda", menu=menu3)
+        menu3.add_cascade(label="Acerca de", command=self.mostrar_autores)
 
-        menu3.add_cascade(label="Acerca de", command=mostrarAutores)
-
+    def create_main_frame(self):
         frame = Frame(self)
-        frame.pack(anchor="center",expand=True)
-        # frame.pack_propagate(False)
+        frame.pack(anchor="center", expand=True)
 
         bienvenida_label = Label(frame, text="¡Bienvenidos a la ventana principal, saludan Y&R!", font=("Trebuchet MS", 14), fg="black", bg="gray")
         bienvenida_label.pack(padx=10, pady=10)
 
         self.imagen = PhotoImage(file="/home/Rick/GitHub/practica-2-grupo-dos-equipo-9/src/gestorGrafico/Imagenes/imgVentPrincipal.png")
-        figura = Label(frame,height=108,width=192,image=self.imagen)
+        figura = Label(frame, height=108, width=192, image=self.imagen)
         figura.pack(anchor="center")
 
         informacion = """
@@ -125,10 +67,43 @@ Estamos comprometidos con brindarte una experiencia fácil de usar y confiable. 
         Para comenzar, selecciona la opción deseada en el menú principal en Procesos y Consultas y sigue las instrucciones que se te presenten.
 
         ¡Disfruta de la experiencia de matricularte de forma rápida y eficaz!
-
         """
-        
-        info_text = Label(frame, text=informacion, font=("Arial", 10), fg="white",bg="black")
+
+        info_text = Label(frame, text=informacion, font=("Arial", 10), fg="white", bg="black")
         info_text.pack(padx=10, pady=10)
 
-        self.mainloop()
+    def info_basica(self):
+        texto = """Texto de información básica"""
+        messagebox.showinfo("Informacion de la aplicacion", texto)
+
+    def salir(self):
+        self.destroy()
+
+    def mostrar_autores(self):
+        autores = "Autores de la aplicacion:\n\nYiduar Duvier Rangel Quintero\n\nRicardo Esteban Lopera Vasco\n\n"
+        messagebox.showinfo("Autores", autores)
+
+    def matricular_materia(self):
+        self.mata_hijos()
+        MatricularMateria(self).pack()
+
+    def generar_horario(self):
+        self.mata_hijos()
+        GenerarHorario(self).pack()
+
+    def eliminar_agregar_materia(self):
+        self.mata_hijos()
+        EliminarAgregarMateria(self).pack()
+
+    def desmatricular_alumno(self):
+        self.mata_hijos()
+        DesmatricularAlumno(self).pack()
+
+    def becas(self):
+        self.mata_hijos()
+        PostulacionBecas(self).pack()
+
+    def mata_hijos(self):
+        for widget in self.winfo_children():
+            if isinstance(widget, Frame):
+                widget.destroy()
--- src/main.py
+++ src/main.py
@@ -40,4 +40,5 @@ if __name__=="__main__":
 
     Deserializador.deserializarDatos()
 
+
     MainWin()
